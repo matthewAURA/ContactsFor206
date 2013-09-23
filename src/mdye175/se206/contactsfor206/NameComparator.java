6bf6128c007dde1dbdbd237421970857c107a16f
Matthew Dyer <matthew.dyer@aura.org.nz>
2013-09-23 17:31:27 +1200

    tracked new name implementation

---
+++ src/mdye175/se206/contactsfor206/NameComparator.java
@@ -0,0 +1,9 @@
+package mdye175.se206.contactsfor206;
+
+public class NameComparator implements ContactComparator {
+	@Override
+	public int compare(Contact one, Contact two) {
+		return one.getName().compareTo(two.getName());
+	}
+
+}
