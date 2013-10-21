ae6b0ae3369b0e03dc4bf6453d72243ed720a00c
Matthew Dyer <matthew.dyer@aura.org.nz>
2013-10-21 21:12:34 +1300

    added operation for adding new contacts

---
+++ src/mdye175/se206/contactsfor206/activity/EditContactActivity.java
@@ -0,0 +1,113 @@
+package mdye175.se206.contactsfor206.activity;
+
+import java.io.Serializable;
+
+import mdye175.se206.contactsfor206.EditTextArrayAdapter;
+import mdye175.se206.contactsfor206.EditTextParameter;
+import mdye175.se206.contactsfor206.R;
+import mdye175.se206.contactsfor206.R.id;
+import mdye175.se206.contactsfor206.R.layout;
+import mdye175.se206.contactsfor206.R.menu;
+import mdye175.se206.contactsfor206.contact.Contact;
+import mdye175.se206.contactsfor206.contact.ContactDataValue;
+import mdye175.se206.contactsfor206.contact.ContactDataValue.Parameter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class EditContactActivity extends Activity {
+
+	private EditText nameField;
+	private EditText phoneNumberField;
+	private EditText emailField;
+	private EditText addressField;
+	private Contact contact;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_edit_contact);
+		
+		//Get the contact we are displaying from bundle
+		Serializable b = getIntent().getSerializableExtra("contact");
+		this.contact = (Contact) b;
+		
+		ListView fieldsList = (ListView)this.findViewById(R.id.fieldsListView);
+		EditTextArrayAdapter editors = new EditTextArrayAdapter(this, android.R.layout.simple_list_item_1,contact);
+		fieldsList.setAdapter(editors);
+		
+		fieldsList.setFocusable(false);
+		fieldsList.setItemsCanFocus(true);
+		
+		//Add in default fields
+		nameField = new EditText(fieldsList.getContext());
+		phoneNumberField = new EditText(fieldsList.getContext());
+		emailField = new EditText(fieldsList.getContext());
+		addressField = new EditText(fieldsList.getContext());
+		
+		editors.add(new EditTextParameter(nameField,ContactDataValue.Parameter.Name));
+		editors.add(new EditTextParameter(phoneNumberField,ContactDataValue.Parameter.PhoneNumber));
+		editors.add(new EditTextParameter(emailField,ContactDataValue.Parameter.Email));
+		editors.add(new EditTextParameter(addressField,ContactDataValue.Parameter.Address));
+		
+		
+		//Set up the data on the page
+		if (contact.getById(ContactDataValue.Parameter.Name) != null)
+			nameField.setText(contact.getById(ContactDataValue.Parameter.Name).getValue());
+		if (contact.getById(ContactDataValue.Parameter.PhoneNumber) != null)
+			phoneNumberField.setText(contact.getById(ContactDataValue.Parameter.PhoneNumber).getValue());
+		if (contact.getById(ContactDataValue.Parameter.Email) != null)
+			emailField.setText(contact.getById(ContactDataValue.Parameter.Email).getValue());
+		if (contact.getById(ContactDataValue.Parameter.Address) != null)
+			addressField.setText(contact.getById(ContactDataValue.Parameter.Address).getValue());
+		
+		//Set up save button and callback
+		Button saveButton = (Button)findViewById(R.id.saveButton);
+		saveButton.setText("Save");
+	
+		saveButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View view) {
+				Log.i("contacts onclick",contact.getById(ContactDataValue.Parameter.Name).getValue());
+			
+				Intent intent = new Intent();
+				Bundle b = new Bundle();
+				b.putSerializable("contact", contact);
+				intent.putExtras(b);
+				intent.putExtra("delete", contact.hashCode());
+				setResult(Activity.RESULT_OK, intent);
+				finish();
+			}
+			
+			
+			
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_contact, menu);
+		return true;
+	}
+	
+	public void finish(){
+		super.finish();
+	}
+
+
+}
---
+++ src/mdye175/se206/contactsfor206/database/AddContactOperation.java
@@ -0,0 +1,50 @@
+package mdye175.se206.contactsfor206.database;
+
+import java.util.List;
+
+import android.database.Cursor;
+
+public class AddContactOperation implements DatabaseOperation<List> {
+
+	@Override
+	public String getSelection() {
+		return null;
+	}
+
+	@Override
+	public String[] getSelectionArgs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGroupBy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHaving() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOrderBy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void doOperation(Cursor input) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List getResults() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
